bbd465697533ea63c6f6cba50deef28b202c7bf6
Forseti17 <yulder17@gmail.com>
2024-08-23 16:50:08 -0500

    sa

---
+++ ejemplo/mvc/MvcDemo.java
@@ -0,0 +1,32 @@
+package ejemplo.mvc;
+
+import ejemplo.controller.ClienteController;
+import ejemplo.model.Cliente;
+import ejemplo.view.ClienteView;
+
+public class MvcDemo {
+	
+	public static void main (String [] args){
+		// objeto vista, y modelo creado con el m�todo est�tico 
+		Cliente modelo= llenarDatosCliente();
+		ClienteView vista= new ClienteView();
+		
+		//se crea un objeto controlador y se le pasa el modelo y la vista
+		ClienteController controlador= new ClienteController(modelo, vista);
+		
+		// se muestra los datos del cliente
+		controlador.actualizarVista();
+		
+		// se actualiza un cliente y se muestra de nuevo los datos
+		controlador.setNombre("Luis");
+		controlador.actualizarVista();		
+	}
+	//m�todo est�tico que retorna el cliente con sus datos
+	private static Cliente llenarDatosCliente() {
+		Cliente cliente = new Cliente();
+		cliente.setId(1);
+		cliente.setNombre("Elivar");
+		cliente.setApellido("Largo");
+		return cliente;
+	}
+}
